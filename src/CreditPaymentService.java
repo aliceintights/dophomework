8825eb613e23d7bb30c06c5047f217d0312d53e1
irina <obesnyuk@gmail.com>
2023-07-19 02:03:51 +0300

    code changed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,16 +1,29 @@
 public class CreditPaymentService {
     public int calculate(int monthlyPercentage) {
         int creditAmount = 1_000_000; // сумма кредита
-        int months = 12;
+        int monthsYear = 12;
+        int months = 10;
+
+        if (months < 12){
+            System.out.println(months = 12);
+            int resultM = 12;
+        } else if (months < 24) {
+            System.out.println(months = 24);
+            int resultM = 24;
+        } else if (months < 36) {
+            System.out.println(months = 36);
+            int resultM = 36;
+        }
+
+        int resultM = months;
         double percentage = 9.99;
-        double monthlyPercent = percentage / months / 100;
-        double x = monthlyPercent * Math.pow((1 + 0.008325), 12);
-        double y = Math.pow((1 + 0.008325), 12) - 1;
+        double monthlyPercent = percentage / monthsYear / 100;
+        double x = monthlyPercent * Math.pow((1 + 0.008325), resultM);
+        double y = Math.pow((1 + 0.008325), resultM) - 1;
         double end = x / y;
-        int creditPayment = 0;
+        int creditPayment = 1;
         int result = creditPayment;
         result = (int) (creditAmount * (double) end);
         return result;
-
     }
 }
